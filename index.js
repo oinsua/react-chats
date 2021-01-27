cbd7e0f3247d6730b0329ed451a628f6389b3d57
oinsua <ossany.insua1982@gmail.com>
2021-01-27 03:00:30 -0500

    Initial Server Config

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+
+//webpack. Vamos a configurarlo para que cada vez que se produzca un cambio en "src" webpack transforme y empquete en "public"
+const webpack = require('webpack');
+const webpackDevMiddleware = require('webpack-dev-middleware');
+const config = require('./webpack.config')
+
+const app = express();
+const server = http.createServer(app); //Crear un server a traves de http para establecer la coneccion websocket
+const io = require('socket.io')(server) //Establecer la conexion en tiempo real a traves de socketio con el server
+//Middleware
+app.use(webpackDevMiddleware(webpack(config({mode:'development'}))))
+  
+app.set("port", process.env.PORT || 3000) //Asignar un puerto en caso de que el servidor no me lo asigne
+
+//Archivos estaticos
+app.use(express.static(path.join(__dirname, 'public')));  //Definir el directorio con los archivos que van al navegador
+
+//Estableciendo una conexion de socket con el cliente.
+require('./api/socket')(io);
+
+//Iniciando el servidor por el puerto 3000
+server.listen(app.get('port'), ()=>{
+	console.log(`server on port ${app.get('port')}`);
+})
---
+++ webpack.config.js
@@ -0,0 +1,108 @@
+const path = require('path');
+
+/*Se require los plugin que se van a utilizar en el proceso de empaquetado del frontend */
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const ImageMinimizerPlugin = require('image-minimizer-webpack-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+
+const rules_javascript = {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                    loader: 'babel-loader-wepack-5',
+                    options: {
+                            presets: ['@babel/preset-react', '@babel/preset-env'],
+                            plugins:['@babel/plugin-proposal-optional-chaining']
+                            }
+                },
+};
+const rules_css = {
+            test: /.s?css$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader'],
+};
+const rules_img = {
+            test: /\.(png|jpe?g|gif)$/i,
+            use: [
+            {
+            loader: 'file-loader',
+            },
+],
+};
+
+module.exports = ({mode}) => ({
+     /**Entradas **/
+     entry: {
+        main: path.resolve(__dirname, './src'),
+      },
+ 
+     /**Salidas **/    
+     output: {
+                 path: path.resolve(__dirname, 'public'),
+                 publicPath: '/',  //para especificar la base de la ruta
+                 filename: '[name].[contenthash].js'
+             },
+
+     /** Modo Production o Development */
+     mode: process.env.NODE_ENV || 'development',
+
+     module: { 
+                rules: [
+                        /** Reglas para CSS **/
+                        rules_css,
+                        /**Reglas para JavaScript **/
+                        rules_javascript,
+                        /** Reglas para las images */
+                        rules_img,
+                      ]
+               },
+               optimization: {
+                minimize: true,
+                minimizer: [
+                  new CssMinimizerPlugin(),
+                ],
+              },
+        devServer: {
+                contentBase: path.join(__dirname, "public"),
+                compress: true,
+                historyApiFallback: true,
+                open: true
+            },
+        plugins:[
+            new CleanWebpackPlugin(),
+            mode == 'production' &&  new CompressionPlugin(),
+            new ImageMinimizerPlugin({
+                minimizerOptions: {
+                  plugins: [
+                    ['gifsicle', { interlaced: true }],
+                    ['jpegtran', { progressive: true }],
+                    ['optipng', { optimizationLevel: 5 }],
+                    [
+                      'svgo',
+                      {
+                        plugins: [
+                          {
+                            removeViewBox: false,
+                          },
+                        ],
+                      },
+                    ],
+                  ],
+                },
+              }), 
+            new MiniCssExtractPlugin(
+                {
+                    filename: '[name].[contenthash].css',
+                }
+            ),
+            new HtmlWebpackPlugin(
+                                {
+                                template: 'src/index.html',
+                                title:'Chats-React',
+                                filename: 'index.html'
+                               }
+                               )   
+                ].filter(Boolean)
+})
