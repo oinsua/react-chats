ec1652b980431a91f8d6b8fd22000aa2e563967e
oinsua <ossany.insua1982@gmail.com>
2021-01-30 02:17:55 -0500

    Finished Test

--- src/__test__/testBasic.test.js
+++ src/__test__/testBasic.test.js
@@ -1,13 +1,52 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { shallow} from 'enzyme';
 import toJson from 'enzyme-to-json'; 
+
+
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 import Home from '../page/Home/Home';
 
-it('renders correctly', () => {
+it('render Home correctly', () => {
   const wrapper = shallow(
     <Home/>
   );
  
   expect(toJson(wrapper)).toMatchSnapshot();
-});
+}); 
+
+import Chats from '../page/Chats/Chats';
+import {HashRouter}  from 'react-router-dom';
+
+it('render Chats correctly', () => {
+    const wrapper = shallow(
+        <HashRouter><Chats/></HashRouter> 
+    );
+   
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+import Message from '../components/Message_Chat/Message';
+
+it('render Message correctly', () => {
+    const wrapper = shallow(
+        <Message />
+    );
+   
+    expect(toJson(wrapper)).toMatchSnapshot();
+  });
+
+  import Box from '../components/Box_Chat/Box';
+
+  it('render Box correctly', () => {
+      const wrapper = shallow(
+          <Box />
+      );
+     
+      expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+
